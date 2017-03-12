980958c01c89e1dcae651ecf684eca8b60aac0df
Pouramua <poukupenga@gmail.com>
2017-03-13 12:04:50 +1300

    Getting familliar

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  people: [
+    {name: 'Pou', age: 19},
+    {name: 'Rua', age: 22},
+    {name: 'Noema', age: 1}
+  ]
+}
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-var React = require('react')
 var ReactDOM = require('react-dom')
+var People = require('./components/People')
+var data = require('./data')
 
-
-function helloTemplate (props) {
-  return (
-    <div>hello {props.name}</div>
-  ) 
-}
-
-var data = { name: 'mix' }
-var view = helloTemplate(data)
+var view = People(data)
 
 var placeToMount = document.getElementById('root')
 
 ReactDOM.render(view, placeToMount)
-
