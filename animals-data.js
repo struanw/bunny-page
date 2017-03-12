45d88205d73766dc18bc77aa98386e3204692558
AidanNairn <aidan.nairn95@gmail.com>
2017-03-13 12:21:27 +1300

    place data within a new file.

---
+++ animals-data.js
@@ -0,0 +1,10 @@
+var animals = {
+  details: [
+    { name: 'ralph', animal: 'dog', age: '6' },
+    { name: 'buffy', animal: 'cat', age: '4' }
+  ]
+}
+
+module.exports = {
+  animals: animals
+}
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+var data = require("./animals-data.js")
+console.log(data);
 
-
-function helloTemplate (props) {
+function animalsTemplate (props) {
   return (
-    <div>hello {props.name}</div>
-  ) 
+    <div>
+      {props.details.map(function(detail){
+        return (
+          <div>{detail.name} is a {detail.species} and is {detail.age} years old.</div>
+        )
+      })}
+    </div>
+  )
 }
 
-var data = { name: 'mix' }
-var view = helloTemplate(data)
+// function Heading (props) {
+//   return (
+//     <h1>Animal Information!</h1>
+//   )
+// }
+//
+// function animalInfo (props) {
+//   return (
+//
+//   )
+// }
+
+var view = animalsTemplate(data.animals)
 
 var placeToMount = document.getElementById('root')
 
 ReactDOM.render(view, placeToMount)
-
